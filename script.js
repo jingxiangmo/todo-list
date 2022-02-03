91df2c4235b84caf9d20d54411303884e0de98f4
Jingxiang Mo <jingxiangmo@gmail.com>
2022-02-03 12:40:44 -0500

    Enter to add functionality added

--- script.js
+++ script.js
@@ -3,18 +3,14 @@ let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
-
-function addItem(paragraph){
-    toDoContainer.appendChild(paragraph);
-    inputField.value = ""; 
-}
-
-
-addToDoButton.addEventListener('click', function(){
+// add a p item
+function addItem(){
     let paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling'); // add paragraph styling
+    
     paragraph.innerText = inputField.value;
-    addItem(paragraph);
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
     
     paragraph.addEventListener('click', function(){
         paragraph.style.textDecoration = "line-through";
@@ -22,4 +18,22 @@ addToDoButton.addEventListener('click', function(){
     paragraph.addEventListener('dblclick', function(){
         toDoContainer.removeChild(paragraph);
     })
-})
+
+}
+// add item on click
+addToDoButton.addEventListener('click', function(){
+    addItem();
+});
+
+// add item on enter
+inputField.addEventListener('keypress', function(event){
+    if(event.keyCode === 13){
+        addItem();
+    }
+});
+
+
+
+
+
+
