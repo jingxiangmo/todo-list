7aaae243a161ffad853ee71b37a6d1b5d6071cfb
Jingxiang Mo <jingxiangmo@gmail.com>
2022-02-03 12:20:19 -0500

    Added style and additional functionalities

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
+
+function addItem(paragraph){
+    toDoContainer.appendChild(paragraph);
+    inputField.value = ""; 
+}
+
+
 addToDoButton.addEventListener('click', function(){
     let paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling'); // add paragraph styling
     paragraph.innerText = inputField.value;
-    
-    toDoContainer.appendChild(paragraph);
-    inputField.value = "";
+    addItem(paragraph);
     
     paragraph.addEventListener('click', function(){
         paragraph.style.textDecoration = "line-through";
